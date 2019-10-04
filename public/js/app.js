bd47b281043d487135e090ae9bad4237edfe3263
Satyaki-Roy <satyakiroy001@gmail.com>
2019-10-04 19:30:26 +0530

    added devDependencies and dev script

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,9 @@
 console.log("Client side JavaScript file is loaded");
 
-// fetch("http://localhost:3000/weather?address=Bengaluru").then(response => {
-//   response.json().then(data => {
-//     if (data.error) {
-//       console.log(data.error);
-//     } else {
-//       console.log(data.location);
-//       console.log(data.forecast);
-//     }
-//   });
-// });
-
 const weatherForm = document.querySelector("form");
-const search = document.querySelector('input');
-const messageOne = document.querySelector('#message-1');
-const messageTwo = document.querySelector('#message-2');
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
 messageOne.textContent = "";
 
@@ -23,17 +12,17 @@ messageOne.textContent = "";
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   const location = search.value;
-  messageOne.textContent = 'Loading...';
-  messageTwo.textContent = '';
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
   // console.log(location);
-    fetch("/weather?address="+location).then(response => {
-        response.json().then(data => {
-            if (data.error) {
-                messageOne.textContent = data.error;
-            } else {
-                messageOne.textContent = data.location;
-                messageTwo.textContent = data.forecast;
-            }
-        });
+  fetch("/weather?address=" + location).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
     });
+  });
 });
