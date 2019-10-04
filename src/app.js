4cdf112a4dc178fdc28619b322d9f00aa5d2830c
Satyaki-Roy <satyakiroy001@gmail.com>
2019-10-04 14:21:50 +0530

    init Commit

---
+++ src/app.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+
+const app = express();
+
+// Defines path for Express Config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//setup handlebars engine and view location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+//setup static directory to server
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Satyaki Roy"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Satyaki Roy"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Satyaki Roy"
+  });
+});
+
+//API consumption
+const geoCode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+app.get("/weather", (req, res) => {
+
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an Address"
+    });
+  }
+
+  geoCode(req.query.address, (error, { Latitude, Longitude, Location } = {}) => {
+    if (error) {
+      return res.send({error});
+    }
+
+    forecast(Latitude, Longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({error});
+      }
+      console.log(Location);
+      console.log(forecastData);
+      res.send({
+        forecast: forecastData,
+        location: Location,
+        addressGiven: req.query.address
+      });
+    });
+  });
+
+  // console.log("req.query is : " + req.query);
+  // res.send({
+  //   forecast: 'It is snowing',
+  //   location: 'India',
+  //   address: req.query.address
+  // });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search team"
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: []
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("error404", {
+    title: "ERROR",
+    error: "Help article not found",
+    name: "Satyaki Roy"
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("error404", {
+    title: "ERROR",
+    error: "Page not found",
+    name: "Satyaki Roy"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const forecast = (longitude = 0, latitude = 0, callback) => {
+  const url =
+    "https://api.darksky.net/forecast/24d840cce813fe2e9ad164db96f88780/" +
+    encodeURIComponent(longitude) +
+    "," +
+    encodeURIComponent(latitude) +
+    "?units=si";
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("No Network Connectivity", undefined);
+    } else if (body.code) {
+      callback("Wrong Input", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} Degree Celsius out. There is a ${body.currently.precipProbability} % chance of rain`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
