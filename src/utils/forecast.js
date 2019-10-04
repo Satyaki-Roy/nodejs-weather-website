495a686d47760c5d7922a85a514db71663186b37
Satyaki-Roy <satyakiroy001@gmail.com>
2019-10-04 19:06:39 +0530

    Added high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const forecast = (longitude = 0, latitude = 0, callback) => {
     } else {
       callback(
         undefined,
-        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} Degree Celsius out. There is a ${body.currently.precipProbability * 100} % chance of rain`
+        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} Degree Celsius out. The highest and the lowest temperature today is ${body.daily.data[0].temperatureHigh} & ${body.daily.data[0].temperatureLow} Degree Celsius. There is a ${body.currently.precipProbability * 100} % chance of rain.`
       );
     }
   });
